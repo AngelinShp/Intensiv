6cbf261b7522dbc2c693404302877f11c5c77642
AngelinShp <angelinashpspb@gmail.com>
2023-09-04 14:34:13 +0300

    Изменения в файловой системе + дз (4_PHP) + файлы на занятие 5

---
+++ Module3/5_PHP_next/index.php
@@ -0,0 +1,10 @@
+<?php
+	echo 'Hello, world!';
+	print 'Hello, world!';
+?>
+<?= 'Hello, world' 
+	/* короткий echo-тег, который является сокращением для 
+	более многословного <?php echo ?> */
+?>
+
+<? echo 'Hello, world!'; // сокращённая версия   ?> 
