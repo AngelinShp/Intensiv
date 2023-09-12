ffe51ded3f755ce8869191d6b1b88232be0cad28
AngelinShp <angelinashpspb@gmail.com>
2023-09-12 15:04:49 +0300

    7_OOP_files (должны быть в domains)

---
+++ Module3/7_PHP_ООП_next/inheritance/index.php
@@ -0,0 +1,27 @@
+<?php
+class Animal 
+{
+	protected $roar = 'Rrr'; // рык
+	public function bark() {
+		echo ' ' . $this->roar;
+	}
+}
+
+class Cat extends Animal
+{
+	protected $roar = 'Meow';
+}
+
+class Dog extends Animal
+{
+	protected $roar = 'Guau';
+}
+
+$animal = new Animal();
+$animal->bark();
+
+$cat = new Cat();
+$cat->bark();
+$dog = new Dog();
+$dog->bark();
+?>
