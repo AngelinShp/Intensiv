74a918350775258bc1570288e5a9cefccf4560df
AngelinShp <angelinashpspb@gmail.com>
2023-09-26 17:58:03 +0300

    3_AJAX (ДЗ ;-))

---
+++ Module4/meeting3/dz_json_encode.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER["CONTENT_TYPE"] == 'application/json')
+{
+    $postData = file_get_contents('php://input');
+    $data = json_decode($postData, true);
+    echo json_encode($data);
+}
+?>
